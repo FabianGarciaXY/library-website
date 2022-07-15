24f3fc12e8679a2255162c373665cfb0b0829276
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-07-15 01:04:33 -0500

    Created controller functions to handle requests for author paths

---
+++ controllers/authorController.js
@@ -0,0 +1,36 @@
+const Author = require('../models/author');
+
+// Render All list of authors
+exports.authorList = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author list');
+};
+
+// Render Information from a specific author
+exports.author = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+};
+
+// Display Author create on GET
+exports.authorCreateGet = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author create GET', );
+};
+
+// Handle author create on POST
+exports.authorCreatePost = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author create POST');
+};
+
+// Display Author delete form on GET.
+exports.authorDeletePost = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author delete GET');
+};
+
+// Display Author update form on GET.
+exports.authorUpdateGet = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author update GET');
+}
+
+// Handle Author update on POST.
+exports.authorUpdatePost = (req, res) => {
+    res.send('NOT IMPLEMENTED: Author update POST');
+};
