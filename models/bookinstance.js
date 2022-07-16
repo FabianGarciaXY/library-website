d1060c74b5a73b9ce495a5ec1aa76e7509b2f561
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-07-15 20:11:06 -0500

    Updated date format for due_date with luxon

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 const Schema = mongoose.Schema;
 
 // BookInstance Schema
@@ -8,7 +9,7 @@ const BookInstanceSchema = new Schema({
     status: {type: String, required: true, 
         enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'
         },  
-    due_back: {type: Date, default: Date.now},
+    due_back: {type: Date, default: Date.now    },
 });
 
 // Virtual for book instance URL.
@@ -18,6 +19,12 @@ BookInstanceSchema
         return '/catalog/bookinstance/' + this._id;
 });
 
+// Virtual for due date
+BookInstanceSchema
+    .virtual('due_back_formatted')
+    .get( function() {
+        return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+    });
 
 // Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
