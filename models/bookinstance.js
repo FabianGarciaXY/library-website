e500853ee57dd1c3d7963b5bf598f1b17d3097b4
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-07-22 17:42:08 -0500

    Added virtual to get date in format yyyy-dd-mm

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -24,7 +24,14 @@ BookInstanceSchema
     .virtual('due_back_formatted')
     .get( function() {
         return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
-    });
+});
+
+// Virtual for due date in forma yyyy-dd-mm
+BookInstanceSchema
+    .virtual('due_back_formatted_yyyy_dd_mm')
+    .get( function() {
+        return this.due_back ? this.due_back.toISOString().substring(0, 10) : '';
+});
 
 // Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
